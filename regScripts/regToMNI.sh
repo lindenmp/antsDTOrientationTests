0beaec16cb1d7af1401d4956240e41c5837e8b11
Linden Parkes <lindenparkes@gmail.com>
2020-02-21 14:10:33 -0500

    Script to register FA to MNI and move DT

---
+++ regScripts/regToMNI.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+binDir=`dirname $0`
+
+if [[ $# -eq 0 ]]; then
+    echo " 
+  $0 <moving series> 
+
+  Run this from the directory above scripts/ . Output is to reg/ .
+
+  Uses ANTs with ANTSPATH=${ANTSPATH}
+
+"
+    exit 1
+
+fi
+
+movingSeries=$1
+
+movingFA="ants/${movingSeries}/${movingSeries}_FA.nii.gz"
+movingDT="ants/${movingSeries}/${movingSeries}_DT.nii.gz"
+
+fixedT1="/Users/lindenmp/Dropbox/Work/git/antsDTOrientationTests/FSL_HCP1065_FA_2mm.nii.gz"
+
+if [[ !(-f $fixedT1 && -f $movingFA) ]] ; then
+    echo " Missing input images "
+    exit 1
+fi
+
+
+outputRoot="reg/${movingSeries}ToMNI/${movingSeries}ToMNI_"
+
+regScripts/regHelper.sh $movingFA $movingDT $fixedT1 $outputRoot
